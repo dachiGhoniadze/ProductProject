c4f5c7c1e0c374c344afbd8f878e5e749ad5bcb1
Dachi Ghoniadze <dachi.ghoniadze@gmail.com>
2024-07-16 11:54:17 +0400

    added project

---
+++ ProductProject/Product.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductProject
+{
+    public class Product
+    {
+        public string Name;
+        public int UniqueNumber;
+
+        private decimal _unitPrice;
+        private string _unitSize;
+        private DateTime _releaseDate;
+        private int _term;
+        private int[] _discountSequence;
+        public Product(string name, int uniqueNumber, decimal unitPrice, string unitSize, DateTime releaseDate, int term)
+        {
+            Name = name;
+            UniqueNumber = uniqueNumber;
+            _unitPrice = unitPrice;
+            _unitSize = unitSize;
+            _releaseDate = releaseDate;
+            _term = term;
+            _discountSequence = new int[12];
+            GenerateRandomDiscounts();
+        }
+        private void GenerateRandomDiscounts()
+        {
+            Random random = new Random();
+            for (int i = 0; i < _discountSequence.Length; i++)
+            {
+                _discountSequence[i] = random.Next(10, 51);
+            }
+        }
+        public int GetDiscountForMonth(int month)
+        {
+            if (month < 1 || month > 12)
+            {
+                throw new ArgumentOutOfRangeException(nameof(month), "Month must be between 1 and 12.");
+            }
+            return _discountSequence[month - 1];
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Unique Number: {UniqueNumber}");
+            Console.WriteLine($"Unit Price: {_unitPrice:C}");
+            Console.WriteLine($"Unit Size: {_unitSize}");
+            Console.WriteLine($"Release Date: {_releaseDate.ToShortDateString()}");
+            Console.WriteLine($"Term: {_term} days");
+
+            Console.WriteLine("Discounts:");
+            for (int i = 0; i < _discountSequence.Length; i++)
+            {
+                Console.WriteLine($"  Month {i + 1}: {_discountSequence[i]}%");
+            }
+        }
+        public bool IsExpired
+        {
+            get
+            {
+                return DateTime.Now > _releaseDate.AddDays(_term);
+            }
+        }
+        
+        public decimal UnitPrice
+        {
+            get { return _unitPrice; }
+        }
+
+        public DateTime GetReleaseDate()
+        {
+            return _releaseDate;
+        }
+
+
+    }
+}
---
+++ ProductProject/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.WriteLine("Enter amount of products: ");
+            int numOfProducts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfProducts; i++)
+            {
+                Console.WriteLine($"Enter details for product {i + 1}:");
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Unique Number: ");
+                int uniqueNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("Unit Price: ");
+                decimal unitPrice = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Unit Size: ");
+                string unitSize = Console.ReadLine();
+
+                Console.Write("Release Date, enter in this format (yyyy-mm-dd): ");
+                DateTime releaseDate = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Term (number of days): ");
+                int term = int.Parse(Console.ReadLine());
+
+                Product product = new Product(name, uniqueNumber, unitPrice, unitSize, releaseDate, term);
+                products.Add(product);
+            }
+
+            Console.WriteLine("\nProducts Info:");
+            foreach (Product product in products)
+            {
+                product.PrintData();
+                Console.WriteLine($"Expired: {(product.IsExpired ? "Yes" : "No")}");
+                Console.WriteLine();
+            }
+
+
+            var expired = products
+                .Where(p => p.IsExpired)
+                .GroupBy(p => p.UniqueNumber)
+                .Select(g => new {
+                    UniqueCode = g.Key,
+                    Expired = g.Count()
+                })
+                .ToList();
+
+            int sumOfExpired = expired.Sum(g => g.Expired);
+
+            Console.WriteLine("============================");
+            Console.WriteLine($"Vadagasuli produktebis jami {sumOfExpired}");
+            Console.WriteLine("Vadagasuli produktebis kodebi");
+            foreach (var group in expired)
+            {
+                Console.WriteLine($"Unique Code: {group.UniqueCode}");
+            }
+
+            var topFive = products
+                .OrderByDescending(p => p.UnitPrice)
+                .Take(5);
+            Console.WriteLine("============================");
+            Console.WriteLine("Top Five expensive products");
+            foreach (var product in topFive)
+            {
+                product.PrintData();
+                Console.WriteLine();
+            }
+
+            var cheapestProduct = products
+                .Where(p => p.GetReleaseDate().Month == 12)
+                .OrderBy(p => p.UnitPrice) 
+                .FirstOrDefault();
+
+            Console.WriteLine("============================");
+            Console.WriteLine("Davaleba #3");
+            if (cheapestProduct != null)
+            {
+                Console.WriteLine("Most cheapest product in December");
+                Console.WriteLine($"Unique Code: {cheapestProduct.UniqueNumber}");
+                Console.WriteLine($"Name: {cheapestProduct.Name}");
+                int stock = products.Count();
+                Console.WriteLine($"Amount of stock of all products: {stock}");
+            }
+            else
+            {
+                Console.WriteLine("Dekemberi carieli!");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
